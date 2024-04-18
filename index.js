d3c6a5a51800f8c7179ab06e7a0a2caad02a239c
Blue-Tides <71620668+Blue-Tides@users.noreply.github.com>
2024-04-18 16:28:48 -0700

    check for undefined + formatting

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { Client, Events, GatewayIntentBits, Collection, EmbedBuilder } = require("discord.js");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent] });
-const {token,apitoken}= require("./config.json");
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const { token, apitoken } = require("./config.json");
 const fs = require('node:fs');
 const path = require('node:path');
-const request=require("request");
+const request = require("request");
 client.commands = new Collection();
 
 const foldersPath = path.join(__dirname, 'commands');
@@ -30,12 +30,12 @@ require("./load_commands.js");
 //fs storage stuff setup
 client.login(token);
 client.on('ready', () => {
-  console.log(client.user.username+" is on!");
+	console.log(client.user.username + " is on!");
 });
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
- // console.log(interaction.client);
+	// console.log(interaction.client);
 	const command = interaction.client.commands.get(interaction.commandName);
 
 	if (!command) {
@@ -56,31 +56,33 @@ client.on(Events.InteractionCreate, async interaction => {
 });
 
 client.on("messageCreate", (message) => {
-	if(message.author.bot) return;
-	if(Math.random()<0.9) return;
-	const m=message.content.replace(/[^a-zA-Z\s]/g,"").split(" ");
-	var word=m[Math.floor(Math.random()*m.length)];
+	if (message.author.bot) return;
+	if (Math.random() < 0.9) return;
+	const m = message.content.replace(/[^a-zA-Z\s]/g, "").split(" ");
+	var word = m[Math.floor(Math.random() * m.length)];
 	const options = {
 		method: 'GET',
 		url: `https://wordsapiv1.p.rapidapi.com/words/${word}/synonyms`,
 		headers: {
-		  'X-RapidAPI-Key': apitoken,
-		  'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+			'X-RapidAPI-Key': apitoken,
+			'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
 		}
-	  };
-	  request(options, function (error, response, body) {
-		if (error) {return;}
-		
-		  var syn=JSON.parse(body);
-		  if(response.statusCode!=200) {return;}
-			try {
-			var embed=new EmbedBuilder();
+	};
+	request(options, function (error, response, body) {
+		if (error) { return; }
+
+		var syn = JSON.parse(body);
+		if (response.statusCode != 200) { return; }
+		if (syn.synonymns[0] === undefined) return;
+		try {
+			var embed = new EmbedBuilder();
 			embed.setTitle("New Word");
 			embed.setDescription(`Hey! Did you know a synonymn for \`${syn.word}\` is \`${(syn.synonyms)[0]}\`? <insert add to list or something btn>`);
-		  message.reply({embeds: [embed]});}
-		  catch(e) {
+			message.reply({ embeds: [embed] });
+		}
+		catch (e) {
 			console.error(e);
-		  }
+		}
 	});
-	
+
 });
