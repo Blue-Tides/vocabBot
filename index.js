79d0fa760cebc5372d41d787e74cbc1a7a711e7c
Blue-Tides <71620668+Blue-Tides@users.noreply.github.com>
2024-04-28 18:20:53 -0700

    basic quizzing system

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Events, GatewayIntentBits, Collection, EmbedBuilder } = require("discord.js");
+const { Client, Events, GatewayIntentBits, Collection, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 const { token, apitoken } = require("./config.json");
@@ -6,7 +6,9 @@ const fs = require('node:fs');
 const path = require('node:path');
 const request = require("request");
 client.commands = new Collection();
-
+client.buttons=new Map();
+client.questions=new Collection();
+client.question=0;
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -55,34 +57,103 @@ client.on(Events.InteractionCreate, async interaction => {
 	}
 });
 
-client.on("messageCreate", (message) => {
+client.on("messageCreate", async (message) => {
 	if (message.author.bot) return;
-	if (Math.random() < 0.9) return;
-	const m = message.content.replace(/[^a-zA-Z\s]/g, "").split(" ");
-	var word = m[Math.floor(Math.random() * m.length)];
-	const options = {
-		method: 'GET',
-		url: `https://wordsapiv1.p.rapidapi.com/words/${word}/synonyms`,
-		headers: {
-			'X-RapidAPI-Key': apitoken,
-			'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
-		}
-	};
-	request(options, function (error, response, body) {
-		if (error) { return; }
+	if (Math.random() < 0) return;
+	if (Math.random() < 0) {
+		const m = message.content.replace(/[^a-zA-Z\s]/g, "").split(" ");
+		var word = m[Math.floor(Math.random() * m.length)];
+		const options = {
+			method: 'GET',
+			url: `https://wordsapiv1.p.rapidapi.com/words/${word}/synonyms`,
+			headers: {
+				'X-RapidAPI-Key': apitoken,
+				'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+			}
+		};
+		request(options, function (error, response, body) {
+			if (error) { return; }
 
-		var syn = JSON.parse(body);
-		if (response.statusCode != 200) { return; }
-		if (syn.synonymns[0] === undefined) return;
-		try {
-			var embed = new EmbedBuilder();
-			embed.setTitle("New Word");
-			embed.setDescription(`Hey! Did you know a synonymn for \`${syn.word}\` is \`${(syn.synonyms)[0]}\`? <insert add to list or something btn>`);
-			message.reply({ embeds: [embed] });
-		}
-		catch (e) {
-			console.error(e);
-		}
-	});
+			var syn = JSON.parse(body);
+			//console.log(syn);
+			//console.log(response.statusCode);
+			if (response.statusCode != 200) { return; }
+			if (syn.synonyms===undefined) return;
+			if (syn.synonyms[0] === undefined) return;
+			try {
+				//console.log("nice");
+				var embed = new EmbedBuilder();
+				embed.setTitle("New Word");
+				embed.setDescription(`Hey! Did you know a synonymn for \`${syn.word}\` is \`${(syn.synonyms)[0]}\`? <insert add to list or something btn>`);
+				message.reply({ embeds: [embed] });
+			}
+			catch (e) {
+				console.error(e);
+			}
+		});
+	} else {
+		const embed=new EmbedBuilder();
+		embed.setTitle("POP QUIZ!");
+		const a=new ButtonBuilder()
+			.setCustomId(`Quiz_${client.question}_A`)
+			.setLabel("A")
+			.setStyle(ButtonStyle.Primary);
+		const b=new ButtonBuilder()
+			.setCustomId(`Quiz_${client.question}_B`)
+			.setLabel("B")
+			.setStyle(ButtonStyle.Primary);
+		const c=new ButtonBuilder()
+			.setCustomId(`Quiz_${client.question}_C`)
+			.setLabel("C")
+			.setStyle(ButtonStyle.Primary);
+		const d=new ButtonBuilder()
+			.setCustomId(`Quiz_${client.question}_D`)
+			.setLabel("D")
+			.setStyle(ButtonStyle.Primary);
+		const row = new ActionRowBuilder()
+			.addComponents(a,b,c,d);
+		client.buttons.set(`Quiz_${client.question}_A`,a);
+		client.buttons.set(`Quiz_${client.question}_B`,b);
+		client.buttons.set(`Quiz_${client.question}_C`,c);
+		client.buttons.set(`Quiz_${client.question}_D`,d);
+		client.questions.set(""+client.question,"B");
+		client.question++;
+		message.channel.send({embeds:[embed],components:[row]});
+	}
 
+});
+client.on('interactionCreate', async (interaction)=>{
+	if(interaction.isButton()) {
+        const btnInfo = interaction.customId.split("_");
+
+        try {
+			if(btnInfo[0]=="Quiz") {
+				const correct=client.questions.get(btnInfo[1]);
+				//console.log(correct);
+				if(btnInfo[2]==correct) {
+					/*award points*/
+					interaction.reply("Correct!");
+				} else {
+					interaction.reply("wrong");
+				}
+				//delete buttons
+				//console.log(client.buttons);
+				//console.log(client.buttons.get(`Quiz_${btnInfo[1]}_A`));
+				//client.buttons.get(`Quiz_${btnInfo[1]}_A`).setDisabled(true);
+				client.buttons.delete(`Quiz_${btnInfo[1]}_A`);
+				//client.buttons.get(`Quiz_${btnInfo[1]}_B`).setDisabled(true);
+				client.buttons.delete(`Quiz_${btnInfo[1]}_B`);
+				//client.buttons.get(`Quiz_${btnInfo[1]}_C`).setDisabled(true);
+				client.buttons.delete(`Quiz_${btnInfo[1]}_C`);
+				//client.buttons.get(`Quiz_${btnInfo[1]}_D`).setDisabled(true);
+				client.buttons.delete(`Quiz_${btnInfo[1]}_D`);
+				//console.log(interaction.message);
+				interaction.message.edit({embeds:interaction.message.embeds,content:`Answered! Correct answer is: ${client.questions.get(btnInfo[1])}`,components:[]});
+				client.questions.delete(""+btnInfo[1]);
+			}
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing the button script !', ephemeral: true});
+        }
+    }
 });
